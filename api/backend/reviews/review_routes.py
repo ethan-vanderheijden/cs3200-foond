a32d2ef28f82ac3d64c98d9b6e18e0ed9c36d075
Eli <elipinals12@gmail.com>
2024-08-15 11:36:49 -0400

    Revert "Eli"

--- api/backend/reviews/review_routes.py
+++ api/backend/reviews/review_routes.py
@@ -4,30 +4,20 @@ from backend.db_connection import get_cursor
 reviews = Blueprint("reviews", __name__, url_prefix="/reviews")
 
 
-@reviews.route("/<cust_id>/<seq_num>", methods=["POST", "DELETE"])
+@reviews.route("/<cust_id>/<seq_num>", methods=["POST"])
 def reviews_info(cust_id, seq_num):
     data = request.json
-    if request.method == "POST":
-        with get_cursor() as cursor:
-            cursor.execute(
-                f"""
-                insert into Recommendation_Review (custId, seqNum, dietScore, priceScore, cuisineScore, formalityScore, locationScore, comment)
-                values ({cust_id}, {seq_num}, {data["dietScore"] or "null"},
-                    {data["priceScore"] or "null"}, {data["cuisineScore"] or "null"},
-                    {data["formalityScore"] or "null"}, {data["locationScore"] or "null"},
-                    '{data["comment"]}')
-                """
-            )
-            return ""
-    elif request.method == "DELETE":
-        with get_cursor() as cursor:
-            cursor.execute(
-                f"""
-                delete from Recommendation_Review where custId = %(cust)s and seqNum = %(seq)s
-                """,
-                {"cust": cust_id},
-                {"seq": seq_num},
-            )
-            return ""
+
+    with get_cursor() as cursor:
+        cursor.execute(
+            f"""
+            insert into Recommendation_Review (custId, seqNum, dietScore, priceScore, cuisineScore, formalityScore, locationScore, comment)
+            values ({cust_id}, {seq_num}, {data["dietScore"] or "null"},
+                {data["priceScore"] or "null"}, {data["cuisineScore"] or "null"},
+                {data["formalityScore"] or "null"}, {data["locationScore"] or "null"},
+                '{data["comment"]}')
+            """
+        )
+        return ""
 
     current_app.logger.info(data)
--- app/src/pages/11_Analytics.py
+++ app/src/pages/11_Analytics.py
@@ -1,26 +1,34 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 import streamlit as st
+from modules.nav import SideBarLinks
 import requests
 
-st.title("Restaurant Analytics")
+st.set_page_config(layout="wide")
 
-# Select category
-category = st.selectbox("Select Category", ["price", "cuisine", "formality"])
+# Display the appropriate sidebar links for the role of the logged in user
+SideBarLinks()
 
-# Button to fetch data
-if st.button("Get Lowest Average Restaurants"):
-    try:
-        response = requests.get(f"http://api:4000/analytics/{category}")
+st.title("Prediction with Regression")
 
-        if response.status_code == 200:
-            data = response.json()
-            if data:
-                st.write(f"Lowest Average {category.capitalize()} Score Restaurants:")
-                for restaurant in data:
-                    st.write(f"- {restaurant['name']}: {restaurant['avg_score']:.2f}")
-            else:
-                st.write("No data found.")
-        else:
-            st.error(f"Error: {response.json().get('error')}")
+# create a 2 column layout
+col1, col2 = st.columns(2)
 
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+# add one number input for variable 1 into column 1
+with col1:
+    var_01 = st.number_input("Variable 01:", step=1)
+
+# add another number input for variable 2 into column 2
+with col2:
+    var_02 = st.number_input("Variable 02:", step=1)
+
+logger.info(f"var_01 = {var_01}")
+logger.info(f"var_02 = {var_02}")
+
+# add a button to use the values entered into the number field to send to the
+# prediction function via the REST API
+if st.button("Calculate Prediction", type="primary", use_container_width=True):
+    results = requests.get(f"http://api:4000/c/prediction/{var_01}/{var_02}").json()
+    st.dataframe(results)
